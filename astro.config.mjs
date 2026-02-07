5936bd86109f4b5486acf0d14e5803adfe2bbd2e
Vikram Sahu <vikram.sahu@harness.io>
2026-02-07 18:39:53 +0530

    update info

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://ivikramsahu.github.io',
+  site: 'https://ivikramsahu.github.io/vikramsahu-blog',
+  base: '/vikramsahu-blog',
   trailingSlash: 'always',
   integrations: [
     tailwind({
